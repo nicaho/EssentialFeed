325785f86763600a0a172b3b591dfd93e1c2d501
nicaho <rungege520@163.com>
2024-05-03 11:57:29 +0800

    feat：Improve test name

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -25,7 +25,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "http://a-url.com")!
         let (sut, client) = makeSUT(url: url)
         

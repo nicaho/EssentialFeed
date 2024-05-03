a428ad9d559572fe4cbea9a28491d8ed487ca32b
nicaho <rungege520@163.com>
2024-05-03 15:02:56 +0800

    feat：Improve readability with help method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -51,7 +51,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load{ capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -90,6 +90,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
             requestedURLs.append(url)
         }
         
-        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
+        }
     }
 }

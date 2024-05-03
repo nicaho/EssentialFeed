9a7061a43d55b6f5dae6d26ab681829efa770a1a
nicaho <rungege520@163.com>
2024-05-03 10:22:51 +0800

    feat：Create EssentialFeed framework && FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by nicaho on 2024/5/3.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

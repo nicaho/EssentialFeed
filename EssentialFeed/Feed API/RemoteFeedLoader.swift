f89673fc6291710351845ba26a66b852a960ffc6
nicaho <rungege520@163.com>
2024-05-03 11:55:14 +0800

    feat：Move 'RemoteFeedLoader' components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by nicaho on 2024/5/3.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+    
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}

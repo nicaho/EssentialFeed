279a4aa0f805e30f67f6889b0a430ce48d0fc25a
nicaho <rungege520@163.com>
2024-05-05 12:08:55 +0800

    fix：Fix SUT memory leaks

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -134,7 +134,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock {
+        addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
         }
     }

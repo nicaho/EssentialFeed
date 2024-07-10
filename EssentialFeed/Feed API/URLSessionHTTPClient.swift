01899862b5a1390752930b9019ba6a13b4263f53
nicaho <rungege520@163.com>
2024-07-10 08:14:18 +0800

    feat：Replace custom `HTTPClientResult` enum with a nested typealias over the standard `Swift.Result`

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -15,14 +15,14 @@ public class URLSessionHTTPClient: HTTPClient {
     }
     
     private struct UnexpectedValuesRepresentation: Error {}
-    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
 //        let url = URL(string: "http://wrong.com")!
         session.dataTask(with: url) { data, response, error in
             
             if let error = error {
                 completion(.failure(error))
             } else if let data = data, let response = response as? HTTPURLResponse{
-                completion(.success(data, response))
+                completion(.success((data, response)))
             }
             else {
                 completion(.failure(UnexpectedValuesRepresentation()))
--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -196,9 +196,9 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
             messages.map { $0.url }
         }
         
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url: url, completion: completion))
         }
         
@@ -213,7 +213,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                 httpVersion: nil,
                 headerFields: nil)!
             
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }

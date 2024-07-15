5055a82dcc3d7052e2ddbeb51f4a1833473a89c2
nicaho <rungege520@163.com>
2024-07-15 21:48:55 +0800

    feat：Simplify `URLSessionHTTPClient` completion code with the new `Result` APIs

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -19,14 +19,24 @@ public class URLSessionHTTPClient: HTTPClient {
 //        let url = URL(string: "http://wrong.com")!
         session.dataTask(with: url) { data, response, error in
             
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse{
-                completion(.success((data, response)))
-            }
-            else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
+//            if let error = error {
+//                completion(.failure(error))
+//            } else if let data = data, let response = response as? HTTPURLResponse{
+//                completion(.success((data, response)))
+//            }
+//            else {
+//                completion(.failure(UnexpectedValuesRepresentation()))
+//            }
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse{
+                    return (data, response)
+                }
+                else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
         }.resume()
     }
 }

f2d520a772a8c32e0c708d6e44ba7d70f378087d
nicaho <rungege520@163.com>
2024-10-30 00:03:30 +0800

    feat：Move `ErrorView` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by nicaho on 2024/10/30.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}

2cf3e6902937e406b61a6fec2dad5e0d1b490057
nicaho <rungege520@163.com>
2024-10-30 00:01:08 +0800

    feat：Move error view configuration to storyboard

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -65,7 +65,7 @@ extension FeedViewController {
     }
     
     var errorMessage: String? {
-        errorView.message
+        errorView?.message
     }
     
     var isShowingLoadingIndicator: Bool {

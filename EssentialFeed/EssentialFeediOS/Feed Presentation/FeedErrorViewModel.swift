4e1a2a650a24e894e81f9e81a2f04cc9f8fbe5f3
nicaho <rungege520@163.com>
2024-10-30 00:26:42 +0800

    feat：Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by nicaho on 2024/10/30.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}

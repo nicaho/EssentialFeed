0a117d680c8a578e3fe795fb9e7c63a44c18a1ca
nicaho <rungege520@163.com>
2024-10-30 00:13:24 +0800

    feat：Show and hide error message animated

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -14,14 +14,45 @@ public final class ErrorView: UIView {
     
     public var message: String? {
         get {
-            label.text
+            isVisible ? label.text : nil
         }
         set {
-            label.text = newValue
+            setMessageAnimated(newValue)
         }
     }
     
     public override func awakeFromNib() {
         label.text = nil
+    }
+    
+    private var isVisible: Bool {
+        alpha > 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message = message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        label.text = message
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 0
+        } completion: { completed in
+            if completed {
+                self.label.text = nil
+            }
+        }
+
     }
 }
